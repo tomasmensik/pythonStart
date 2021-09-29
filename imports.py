f955b286c799cfa824024d348b6f378822c92c81
tomasmensik <66717897+tomasmensik@users.noreply.github.com>
2021-09-29 22:05:20 +0200

    Add files via upload

--- imports.py
+++ imports.py
@@ -10,11 +10,19 @@ Použijte vhodné moduly v Pythonu (včetně jejich případné instalace) k tom
 K řešení prvního úkolu je možné doporučit importovat interní modul datetime
 Řešení dalších dvou úkolů můžete odvodit z příkladů v dokumentaci k externímu modulu dateutil - viz https://pypi.org/project/python-dateutil/
 """
-import datetime
+from datetime import *
+from dateutil.easter import *
+from dateutil.rrule import *
 
-x = datetime.datetime.today()
+x = datetime.today()
 print("\n1.)Dnešní datum a čas: {}" .format(x))
-y = None
-print("2.)Datum Velikonoční neděle v následujících 5 letech: {}" .format(y))
-z = None
-print("3.)Vypsání nejbližšího roku, v némž je Štědrý den v neděli: {}" .format(z))
+
+y1 = easter(2022,method=3)
+y2 = easter(2023,method=3)
+y3 = easter(2024,method=3)
+y4 = easter(2025,method=3)
+y5 = easter(2026,method=3)
+print("\n2.)Datum Velikonoční neděle v následujících 5 letech: {}, {}, {}, {}, {}" .format(y1,y2,y3,y4,y5))
+
+z = rrule(YEARLY,dtstart=x,bymonth=12,bymonthday=24,byweekday=SU)[0].year
+print("\n3.)Vypsání nejbližšího roku, v némž je Štědrý den v neděli: {}" .format(z))
---
+++ myapp.py
@@ -0,0 +1,69 @@
+print('\n\n\nAhoj, tohle je jednoduchy test vedomosti!')
+print('Zadejte udaje, prosim.')
+print('------------------\n')
+
+hodnoceni = 0
+otazka = 0
+condition = None
+jmeno = input('Vaše jméno: ')
+vek = input('Váš věk: ')
+
+if jmeno.isalpha() == True and vek.isnumeric() == True:
+    condition = False
+else:
+    condition = True
+
+while condition:
+    print('\nŠpatně zadané jméno nebo věk.')
+    jmeno = input('Vaše jméno: ')
+    vek = input('Váš věk: ')
+
+    if jmeno.isalpha() == True and vek.isnumeric() == True:
+        condition = False
+    else:
+        condition = True
+
+print('\nNejvyšší hora na světě?')
+print ('A. Kilimanjaro')
+print ('B. Mont Blanc')
+print ('C. Mount Everest')
+
+x = input('Odpověď: ')
+if x == "c":
+    print('Správně.\n')
+    hodnoceni = hodnoceni + 1
+else:
+    print('Špatně.\n')
+otazka = otazka + 1
+
+print('Žije v Rusku více jak 100mil lidí?')
+print('A. Ano')
+print('B. Ne')
+
+x = input('Odpověď: ')
+if x == "a":
+    print('Správně.\n')
+    hodnoceni = hodnoceni + 1
+else:
+    print('Špatně.\n')
+otazka = otazka + 1
+
+print('Začala První světová válka v roce 1914?')
+print('A. Ano')
+print('B. Ne')
+
+x = input('Odpověď: ')
+if x == "a":
+    print('Správně.\n')
+    hodnoceni = hodnoceni + 1
+else:
+    print('Špatně.\n')
+otazka = otazka + 1
+
+if hodnoceni == 0 or hodnoceni == 1:
+    print('Nic moc {}/e/i/o/u, měl bys na sobě zapracovat, protože už máš {} let.' .format(jmeno, vek))
+elif hodnoceni == 2:
+    print('Jde to {}/e/i/o/u, ale na {} let by to mohlo být lepší.' .format(jmeno, vek))
+else:
+    print('Výborně {}/e/i/o/u, jelikož máš {} let, tak jsi nezklamal.' .format(jmeno, vek))
+print('Vaše hodnocení je: {}/{}' .format(hodnoceni, otazka))
